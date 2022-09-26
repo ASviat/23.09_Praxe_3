179260e4b57ce495465614535e677bc44a98e117
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-26 21:09:34 +0300

    Task 17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите координату точки Х:");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату точки Y:");
+int y=Convert.ToInt32(Console.ReadLine());
+
+int quarter= Quarter(x,y);
+Console.WriteLine(quarter==0?"Введены некорректные координаты":$"Четверть {quarter}");
+
+int Quarter (int X, int Y)
+{
+    if (X>0 && Y>0) return 1;
+    if (X>0 && Y<0) return 2;
+    if (X<0 && Y<0) return 3;
+    if (X<0 && Y>0) return 4;
+    return 0;
+}
