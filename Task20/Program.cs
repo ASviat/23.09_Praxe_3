d78d58c1e8fe2cb4e8bcbd838aef5ac2c364d3a4
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-28 15:27:48 +0300

    sum via return

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -16,8 +16,7 @@ double Distance(int xC1, int yC1, int xC2, int yC2)
     double n1=Math.Pow(xC2-xC1, 2);
     double n2=Math.Pow(yC2-yC1, 2);
     double sum=Math.Sqrt(n1+n2);
-    double Sum=Math.Round(sum,2,MidpointRounding.ToZero);
-    return Sum;
+    return Math.Round(sum,2,MidpointRounding.ToZero);
 }
 
 double distance=Distance(xc1,yc1,xc2,yc2);
