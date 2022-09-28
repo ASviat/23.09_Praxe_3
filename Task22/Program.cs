5ab87db0fe32c277083cfadd05b2df7975476f12
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-28 16:02:54 +0300

    Init Commit

---
+++ Task22/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число, дабы магия вершилась: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int Square(int number)
+{
+    int count=1;
+    while (count<=number)
+    {
+        int scount=count*count;
+        Console.Write($"{scount} ");
+        count++;
+    }
+    return -1;
+}
+int square=Square(num);
+Console.WriteLine($"{square}");
