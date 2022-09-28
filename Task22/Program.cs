fdf7d4d743823e133311e477c7c46c72380f2106
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-28 16:18:05 +0300

    void method

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,16 +1,16 @@
 ﻿Console.WriteLine("Введите число, дабы магия вершилась: ");
 int num=Convert.ToInt32(Console.ReadLine());
 
-int Square(int number)
+void Square(int number)
 {
     int count=1;
     while (count<=number)
     {
-        int scount=count*count;
-        Console.Write($"{scount} ");
+        Console.WriteLine($"{count}  {count*count}");
         count++;
     }
-    return -1;
+    
 }
-int square=Square(num);
-Console.WriteLine($"{square}");
+
+if (num>0)Square(num);
+else Console.WriteLine ("Введите корректные значения");
